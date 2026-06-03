b6926b3e83a2b83a95f381abe6b389c9e55d649d
emilianotorchia20-hub <emilianotorchia20@gmail.com>
2026-06-03 19:11:28 -0300

    FINALIZACION

--- sw.js
+++ sw.js
@@ -1,112 +1,168 @@
-const CACHE_NAME = "gametrack-v3";
+const CACHE_NAME = "gametrack-v1";
 
 const urlsToCache = [
 
-  "/",
-  "/index.html",
+  "./",
+  "./index.html",
 
-  "/PAGES/historial/index.html",
-  "/PAGES/estadisticas/index.html",
-  "/PAGES/configuracion/index.html",
-  "/PAGES/roulette/index.html",
+  "./PAGES/historial/index.html",
+  "./PAGES/estadisticas/index.html",
+  "./PAGES/configuracion/index.html",
+  "./PAGES/roulette/index.html",
 
-  "/CSS/base.css",
-  "/CSS/layout.css",
-  "/CSS/utilities.css",
+  "./CSS/base.css",
+  "./CSS/layout.css",
+  "./CSS/utilities.css",
 
-  "/CSS/components/buttons.css",
-  "/CSS/components/cards.css",
-  "/CSS/components/forms.css",
-  "/CSS/components/navbar.css",
-  "/CSS/components/stats.css",
+  "./CSS/components/buttons.css",
+  "./CSS/components/cards.css",
+  "./CSS/components/forms.css",
+  "./CSS/components/navbar.css",
+  "./CSS/components/stats.css",
 
-  "/JS/app.js",
-  "/JS/storage.js",
-  "/JS/sessions.js",
-  "/JS/roulette.js"
+  "./JS/app.js",
+  "./JS/storage.js",
+  "./JS/sessions.js",
+  "./JS/roulette.js"
 
 ];
 
 // ==========================
 // INSTALL
 // ==========================
-self.addEventListener("install", event => {
 
-  console.log("SW installing...");
+self.addEventListener(
 
-  event.waitUntil(
+  "install",
 
-    caches.open(CACHE_NAME)
+  event => {
 
-      .then(cache => {
+    console.log(
+      "SW installing..."
+    );
 
-        return cache.addAll(urlsToCache);
+    event.waitUntil(
 
-      })
+      caches
+        .open(CACHE_NAME)
 
-  );
+        .then(cache => {
 
-});
+          return cache.addAll(
+            urlsToCache
+          );
+
+        })
+
+    );
+
+  }
+
+);
 
 // ==========================
 // ACTIVATE
 // ==========================
-self.addEventListener("activate", event => {
 
-  console.log("SW activated");
+self.addEventListener(
 
-  event.waitUntil(
+  "activate",
 
-    caches.keys().then(keys =>
+  event => {
 
-      Promise.all(
+    console.log(
+      "SW activated"
+    );
 
-        keys.map(key => {
+    event.waitUntil(
 
-          if(key !== CACHE_NAME){
+      caches.keys()
 
-            return caches.delete(key);
+        .then(keys =>
 
-          }
+          Promise.all(
 
-        })
+            keys.map(key => {
 
-      )
+              if (
+                key !== CACHE_NAME
+              ) {
 
-    )
+                return caches.delete(
+                  key
+                );
 
-  );
+              }
 
-});
+            })
+
+          )
+
+        )
+
+    );
+
+  }
+
+);
 
 // ==========================
 // FETCH
 // ==========================
-self.addEventListener("fetch", event => {
 
-  event.respondWith(
+self.addEventListener(
 
-    caches.match(event.request)
+  "fetch",
 
-      .then(res => {
+  event => {
 
-        return res || fetch(event.request);
+    event.respondWith(
+
+      caches.match(
+        event.request
+      )
+
+      .then(response => {
+
+        return (
+
+          response ||
+
+          fetch(
+            event.request
+          )
+
+        );
 
       })
 
-  );
-
-});
-
-// ==========================
-// 🔥 UPDATE MANUAL
-// ==========================
-self.addEventListener("message", event => {
-
-  if(event.data?.type === "SKIP_WAITING"){
-
-    self.skipWaiting();
+    );
 
   }
 
-});
+);
+
+// ==========================
+// UPDATE MANUAL
+// ==========================
+
+self.addEventListener(
+
+  "message",
+
+  event => {
+
+    if (
+
+      event.data?.type ===
+      "SKIP_WAITING"
+
+    ) {
+
+      self.skipWaiting();
+
+    }
+
+  }
+
+);
